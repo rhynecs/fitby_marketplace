371987445c664ce148d17fb5acb38413ecd8e37e
rhynecs <contact@henrystevens.me>
2022-05-19 13:06:43 +1000

    added page to see individual users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
+  after_create :assign_default_role
+
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  def index
-    @users = User.all
+  def assign_default_role
+    self.add_role(:user) if self.roles.blank?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
   get "/users", to: "users#index"
+  get "/users/:id", to: "users#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
