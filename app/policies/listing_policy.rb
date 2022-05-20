b7c550567513f2c1b0c4b6c165e97d0fcfaa5f49
rhynecs <contact@henrystevens.me>
2022-05-20 21:46:38 +1000

    policy name change

--- app/policies/listing_policy.rb
+++ app/policies/listing_policy.rb
@@ -1,4 +1,4 @@
-class UserPolicy < ApplicationPolicy
+class ListingPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     # def resolve
